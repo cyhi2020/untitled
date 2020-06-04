9038748419f1d243b69979226977994fedb56430
cyhi2020 <cy_hi@163.com>
2020-06-04 17:59:56 +0800

    用于测试加入按钮槽函数而更改了相关代码

--- CallFirstMainWin.py
+++ CallFirstMainWin.py
@@ -2,13 +2,27 @@
 
 import sys 	
 from PyQt5.QtWidgets import QApplication , QMainWindow
-from firstMainWin import *
+#from firstMainWin import *
 #from layoutWin import *
+from layout_demo_LayoutManage import *
+from PyQt5.QtCore import pyqtSlot
 
-class MyMainWindow(QMainWindow, Ui_MainWindow):
+class MyMainWindow(QMainWindow, Ui_LayoutDemo): #Ui_MainWindow
     def __init__(self, parent=None):    
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
+
+    @pyqtSlot()
+    def on_pushButton_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        print('收益_min:', self.doubleSpinBox_returns_min.text())
+        print('收益_max:', self.doubleSpinBox_returns_max.text())
+        print('最大回撤_min:', self.doubleSpinBox_maxdrawdown_min.text())
+        print('最大回撤_max:', self.doubleSpinBox_maxdrawdown_max.text())
+        print('sharp比_min:', self.doubleSpinBox_sharp_min.text())
+        print('sharp比_max:', self.doubleSpinBox_sharp_max.text())
             
 if __name__=="__main__":  
     app = QApplication(sys.argv)  
